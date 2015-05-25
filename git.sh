6e1fdd5d339f23f0878920b14030c051254fc12a
Florian Raux <rauxflorian@yahoo.fr>
2015-05-25 11:46:52 +0200

    initial commit for prefered configurations

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Applying webcodepeeing git configuration for $USER"
+git config --global user.name "Florian Raux"
+git config --global user.email "rauxflorian@yahoo.fr"
+git config --global core.editor vi
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.interactive auto
+git config --global color.diff auto
+git config --global color.ui true
+echo "I bet your the best, git is configured my lord..."
